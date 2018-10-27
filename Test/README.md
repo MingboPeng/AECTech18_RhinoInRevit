38fdd6cfd975daa7e014f9e7486892bf39eeb803
tmwarchitecture <tmwarchitecture@gmail.com>
2018-10-27 17:07:41 -0400

    add test file

---
+++ Test/README.md
@@ -0,0 +1,65 @@
+# Rhino Inside Revit
+The Rhino Inside technology allows Rhino and Grasshopper to be embedded within other products.
+
+This folder contains a sample project that demonstrates:
+
+1. Starting Rhino and Grasshopper as a Revit Addon.
+2. Call directly into the native Revit APIs from a Grasshopper or Rhino plugin.
+2. Grasshopper definitions can be opened and previewed in Rhino within the Revit process.
+3. Object can be natively created by Rhino or Grasshopper within Revit through the API.
+
+[**Questions?** *Visit the discussion forum...*](https://discourse.mcneel.com/c/serengeti/inside)
+
+## Getting started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+* Git client. ([download](https://git-scm.com/downloads))
+* Visual Studio 2017 ([download](https://visualstudio.microsoft.com/downloads/))
+* Rhino WIP ([download](https://www.rhino3d.com/download/rhino/wip))
+* Autodesk Revit 2017-2019 ([download](https://www.autodesk.com/products/revit/free-trial))
+
+### Getting Source & Build
+1. Clone the repository. At the command prompt, enter the following command:
+```
+git clone --recursive https://github.com/mcneel/rhino.inside.git rhino.inside
+```
+2. In Visual Studio: open _rhino.inside\Autodesk\Revit\RhinoInside.Revit.sln_.
+3. Set the Solution Configuration drop-down according the Revit version you have installed. This will properly link the correct Revit API libraries to the project.
+4. Navigate to _Build_ > _Build Solution_ to begin your build.
+
+### Installing & Uninstalling
+The project is configured to copy .addon file as well as output files to the folder _"%APPDATA%\\Autodesk\\Revit\\Addins\\%RevitVer%\\RhinoInside.Revit\\"_ folder in order to make Revit load this addon next time it runs.
+
+In order to uninstall it you can use Visual Studio _Build_ > _Clean Solution_ command or just navigate to the folder _"%APPDATA%\\Autodesk\\Revit\\Addins\\%RevitVer%\\_ and remove the file _RhinoInside.Revit.addon_ and the folder _RhinoInside.Revit_.
+
+## Using the samples
+* Once you launch Revit it will warn you this addon is not verified, please click on "Load allways" or "Load once".
+* Create a new project or open an existing one.
+
+### Sample1
+1. Open the ribbon tab called _Add-Ins_.
+2. Click on the button you will found on the _Rhinoceros_ panel called _Sample 1_.
+3. You should have an sphere in the Revil model.
+
+### Sample2
+![](doc/Sample2.jpg)
+
+1. Rhino window will open on top of Revit window.
+2. Create some breps in Rhino. (It is best if they are building size objects to start.)
+3. Run _Grasshopper_ command.
+4. Open _"Sample2.ghx"_ file in Grasshopper.
+5. Inside Grasshopper using the _Brep_ param pick the breps you created in step 2.
+6. Inside Grasshopper click on the button _Push to Bake_.
+7. You should have the breps created in step 2 in the Revit model.
+
+### Sample3
+![](doc/Sample3.gif)
+
+1. Rhino window will open on top of Revit window.
+2. Create a surface on Rhino. (It is best if it is site size objects to start.)
+4. Run _Grasshopper_ command.
+5. Open _"Sample3.ghx"_ file in Grasshopper.
+6. Inside Grasshopper using the _Surface_ param pick the breps you created in step 2.
+7. Inside Grasshopper click on the button _Update Topography surface_.
+8. You should have a Topography surface in Revit.
---
+++ Test/Revit.cs
@@ -0,0 +1,249 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Media.Imaging;
+
+using Autodesk;
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Autodesk.Revit.ApplicationServices;
+
+using Rhino;
+using Rhino.Runtime.InProcess;
+
+namespace RhinoInside.Revit
+{
+  [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+  [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+  [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+  public class Revit : IExternalApplication
+  {
+    #region Revit static constructor
+    static Revit()
+    {
+      ResolveEventHandler OnRhinoCommonResolve = null;
+      AppDomain.CurrentDomain.AssemblyResolve += OnRhinoCommonResolve = (sender, args) =>
+      {
+        const string rhinoCommonAssemblyName = "RhinoCommon";
+        var assemblyName = new AssemblyName(args.Name).Name;
+
+        if (assemblyName != rhinoCommonAssemblyName)
+          return null;
+
+        AppDomain.CurrentDomain.AssemblyResolve -= OnRhinoCommonResolve;
+
+        string rhinoSystemDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino WIP", "System");
+        return Assembly.LoadFrom(Path.Combine(rhinoSystemDir, rhinoCommonAssemblyName + ".dll"));
+      };
+    }
+    #endregion
+
+    #region IExternalApplication Members
+
+    internal static BitmapImage RhinoLogo = LoadImage("RhinoInside.Resources.Rhino.png");
+
+    private RhinoCore rhinoCore;
+
+    public Autodesk.Revit.UI.Result OnStartup(UIControlledApplication applicationUI)
+    {
+      ApplicationUI = applicationUI;
+
+#if REVIT_2019
+      MainWindowHandle = ApplicationUI.MainWindowHandle;
+#else
+      MainWindowHandle = Process.GetCurrentProcess().MainWindowHandle;
+#endif
+
+      // Load Rhino
+      try
+      {
+        var schemeName = ApplicationUI.ControlledApplication.VersionName.Replace(' ', '-');
+        rhinoCore = new RhinoCore(new string[] { $"/scheme={schemeName}", "/nosplash" }, WindowStyle.Normal, MainWindowHandle);
+      }
+      catch (Exception e)
+      {
+        Debug.Fail(e.Source, e.Message);
+        return Autodesk.Revit.UI.Result.Failed;
+      }
+
+      // Register UI
+      {
+        RibbonPanel ribbonPanel = ApplicationUI.CreateRibbonPanel("Rhinoceros");
+
+        Sample1.CreateUI(ribbonPanel);
+      }
+
+      // Add an Idling event handler to notify Rhino when the process is idle
+      ApplicationUI.Idling += new EventHandler<IdlingEventArgs>(OnIdle);
+
+      return Autodesk.Revit.UI.Result.Succeeded;
+    }
+
+    public Autodesk.Revit.UI.Result OnShutdown(UIControlledApplication applicationUI)
+    {
+      // Remove the Idling event handler
+      ApplicationUI.Idling -= new EventHandler<IdlingEventArgs>(OnIdle);
+
+      // Unload Rhino
+      try
+      {
+        rhinoCore.Dispose();
+      }
+      catch (Exception e)
+      {
+        Debug.Fail(e.Source, e.Message);
+        return Autodesk.Revit.UI.Result.Failed;
+      }
+
+      ApplicationUI = null;
+      return Autodesk.Revit.UI.Result.Succeeded;
+    }
+
+    public void OnIdle(object sender, IdlingEventArgs args)
+    {
+      // 1. Do Rhino pending OnIdle tasks
+      if (rhinoCore.OnIdle())
+      {
+        args.SetRaiseWithoutDelay();
+        return;
+      }
+
+      // Document dependant tasks need a document
+      var doc = (sender as UIApplication)?.ActiveUIDocument?.Document;
+      if (doc == null)
+        return;
+
+      // 2. Do all BakeGeometry pending tasks
+      lock (bakeQueue)
+      {
+        if (bakeQueue.Count > 0)
+        {
+          using (var trans = new Transaction(doc))
+          {
+            if (trans.Start("BakeGeometry") == TransactionStatus.Started)
+            {
+              var genericModelId = new ElementId(BuiltInCategory.OST_GenericModel);
+
+              while (bakeQueue.Count > 0)
+              {
+                var geometryList = bakeQueue.Dequeue();
+                if (geometryList != null)
+                {
+                  try
+                  {
+                    var genericModelList = new List<GeometryObject>();
+
+                    // DirectShape only accepts those types and no nulls
+                    foreach (var g in geometryList)
+                    {
+                      switch (g)
+                      {
+                        case Point p: genericModelList.Add(p); break;
+                        case Curve c: genericModelList.Add(c); break;
+                        case Solid s: genericModelList.Add(s); break;
+                        case Mesh  m: genericModelList.Add(m); break;
+                      }
+                    }
+
+                    if (genericModelList.Count > 0)
+                    {
+                      var ds = DirectShape.CreateElement(doc, genericModelId);
+                      ds.SetShape(genericModelList);
+                    }
+                  }
+                  catch(Exception e)
+                  {
+                    Debug.Fail(e.Source, e.Message);
+                  }
+                }
+              }
+            }
+
+            trans.Commit();
+          }
+        }
+      }
+
+      // 3. Do all document actions
+      lock (documentActions)
+      {
+        if (documentActions.Count > 0)
+        {
+          using (var trans = new Transaction(doc))
+          {
+            var action = documentActions.Peek();
+            if (trans.Start(action.GetMethodInfo().Name) == TransactionStatus.Started)
+            {
+              try
+              {
+                documentActions.Dequeue().Invoke((sender as UIApplication)?.ActiveUIDocument);
+                trans.Commit();
+              }
+              catch (Exception e)
+              {
+                Debug.Fail(e.Source, e.Message);
+                trans.RollBack();
+              }
+            }
+          }
+        }
+
+        if (documentActions.Count > 0)
+          args.SetRaiseWithoutDelay();
+      }
+    }
+
+    private static Queue<IList<GeometryObject>> bakeQueue = new Queue<IList<GeometryObject>>();
+
+
+    public static void BakeGeometry(IEnumerable<Rhino.Geometry.GeometryBase> geometries)
+    {
+      lock (bakeQueue)
+      {
+        foreach (var list in geometries.ToHost())
+          bakeQueue.Enqueue(list);
+      }
+    }
+
+    private static Queue<Action<UIDocument>> documentActions = new Queue<Action<UIDocument>>();
+    public static void EnqueueAction(Action<UIDocument> action)
+    {
+      lock (documentActions)
+        documentActions.Enqueue(action);
+    }
+
+    #endregion
+
+    #region Public Methods
+    public static IntPtr MainWindowHandle { get; private set; }
+    public static UIControlledApplication ApplicationUI { get; private set; }
+
+    public const double ModelAbsoluteTolerance = (1.0 / 12.0) / 16.0; // 1/16 inch in feets
+    public const double ModelAbsolutePlanarTolerance = Revit.ModelAbsoluteTolerance / 10; // in feets
+    public const Rhino.UnitSystem ModelUnitSystem = Rhino.UnitSystem.Feet; // Always feet
+
+    public static double RhinoToRevitModelScaleFactor => RhinoDoc.ActiveDoc == null ? Double.NaN : RhinoMath.UnitScale(RhinoDoc.ActiveDoc.ModelUnitSystem, Revit.ModelUnitSystem);
+    internal static double RhinoModelAbsoluteTolerance => ModelAbsoluteTolerance / RhinoToRevitModelScaleFactor; // in Rhino model units
+    #endregion
+
+    #region Private Methods
+    static private BitmapImage LoadImage(string name)
+    {
+      var bmi = new BitmapImage();
+      bmi.BeginInit();
+      bmi.StreamSource = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+      bmi.EndInit();
+      return bmi;
+    }
+    #endregion
+  }
+
+}
